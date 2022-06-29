normalization bugs fixed

src/altk/language/language.py
@@ -3,10 +3,11 @@
 The base object of altk is a Language. This is intended to model a language scientifically (especially parts of its semantics) and to enable various use cases. Most notably, experiments such as analyses of efficient communication, learnability, automatic corpus generation for ML probing, etc.
 """
 
-from abc import abstractmethod
 import numpy as np
+from abc import abstractmethod
 from altk.language.semantics import Universe
 from altk.language.semantics import Meaning
+from typing import Callable
 
 
 class Expression:
@@ -69,10 +70,6 @@ class Language:
         """Add an expression to the list of expressions in a language."""
         self.expressions = self.expressions + [e]
 
-    # def size(self) -> int:
-    #     """Returns the length of the list of expressions in a language."""
-    #     return len(self.expressions)
-
     def pop(self, index: int) -> Expression:
         """Removes an expression at the specified index of the list of expressions, and returns it."""
         if not self.size():
@@ -86,22 +83,14 @@ class Language:
         """Whether a language represents a human natural language."""
         raise NotImplementedError
 
+    def degree_property(self, property: Callable[[Expression], bool]) -> float:
+        """Count what percentage of expressions in a language have a given property."""
+        return sum([property(item) for item in self.expressions]) / self.len(self)
+
     def get_matrix(self) -> np.ndarray:
         """Get a binary matrix of shape `(num_meanings, num_expressions)`
         specifying which expressions can express which meanings."""
-
-        # expressions = tuple(self.expressions)
-        # meanings = tuple(self.universe.objects)
-
-        # len_e = self.__len__()
-        # len_m = len(meanings)
-
-        # mat = np.zeros((len_m, len_e))
-        # for i, m in enumerate(meanings):
-        # for j, e in enumerate(expressions):
-        # mat[i, j] = float(e.can_express(m))
-
-        return np.ndarray(
+        return np.array(
             [
                 [float(e.can_express(m)) for e in self.expressions]
                 for m in self.universe.objects