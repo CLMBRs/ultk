1ae1d6b2ae260acd275c23fd7148804a1e02de58
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2022-07-01 11:40:57 -0700

    finished dataframe func and removed everything else from analysis

--- src/altk/effcomm/analysis.py
+++ src/altk/effcomm/analysis.py
@@ -6,11 +6,7 @@ import pandas as pd
 from altk.language.language import Language
 
 
-def get_report() -> pd.DataFrame:
-    """Runs statistical tests and returns a dataframe containing correlations of optimality with degrees of naturalness and means of optimality for each natural language."""
-
-
-def get_dataframe(languages: list[Language], columns: list[str] = None, subset: list[str] = None, repeats=None) -> pd.DataFrame:
+def get_dataframe(languages: list[Language], columns: list[str] = None, subset: list[str] = ["complexity", "comm_cost"], duplicates: str = "leave") -> pd.DataFrame:
     """Get a pandas DataFrame for a list of languages containing efficient communication data.
 
     Args:
@@ -20,68 +16,33 @@ def get_dataframe(languages: list[Language], columns: list[str] = None, subset: 
 
         subset: the columns to subset for duplicates
 
+        duplicates: {"drop", "count", "leave"} whether to drop, count, or do nothing with duplicates. By default is set to "leave" which will leave duplicates in the dataframe.
+
     Returns:
         - data: a pandas DataFrame with rows as individual languages, with the columns specifying their data. 
     """
+    if columns is None:
+        columns = list(languages[0].data.keys())
+
     data = pd.DataFrame(
-        data=[(lang.data[k] for k in columns) for lang in languages],
+        data=[tuple(lang.data[k] for k in columns) for lang in languages],
         columns=columns,
     )
 
-    # Pandas confused by mixed types int and string, so convert back.
-    sample = {k:languages[0].data[k] for k in columns}
-    data_types = {
-        "string": [k for k in columns if isinstance(sample[k], str)],
-        "float": [k for k in columns if isinstance(sample[k], float)],
-        "int": [k for k in columns if isinstance(sample[k], int)],
-        "bool": [k for k in columns if isinstance(sample[k], int)],
-    }
-    data[[data_types["float"], data_types["int"]]].apply(pd.to_numeric)
-
     # drop duplicates without counting
-    if repeats == "drop":
+    if duplicates == "drop":
         data = data.drop_duplicates(subset=subset)
 
     # drop but count duplicates
-    elif repeats == "count":
+    elif duplicates == "count":
         vcs = data.value_counts(subset=subset)
         data = data.drop_duplicates(subset=subset)
         data = data.sort_values(by=subset)
         data["counts"] = vcs.values
 
-    elif repeats is not None:
+    elif duplicates not in ["drop", "count", "leave"]:
         raise ValueError(
-            f"the argument `repeats` must be either 'drop' or 'count'. Received: {repeats}"
+            f"the argument `duplicates` must be either 'drop', 'count', 'leave'. Received: {duplicates}"
         )
 
     return data    
-
-
-def get_tradeoff_plot(
-    languages: list[Language], dominating_languages: list[Language]
-) -> pn.ggplot:
-    """Create the main plotnine plot for the communicative cost, complexity trade-off for the experiment.
-
-    Returns:
-        - plot: a plotnine 2D plot of the trade-off.
-    """
-    # data = self.get_dataframe(self.get_languages())
-    data = get_dataframe(languages)
-    pareto_df = get_dataframe(dominating_languages)
-    plot = (
-        pn.ggplot(data=data, mapping=pn.aes(x="comm_cost", y="complexity"))
-        + pn.scale_x_continuous(limits=[0, 1])
-        + pn.geom_jitter(
-            stroke=0,
-            alpha=1,
-            width=0.00,
-            height=0.00,
-            # mapping=pn.aes(size="Language", shape="Language", fill="Language"),
-            mapping=pn.aes(size="Language", shape="Language", color="naturalness"),
-        )
-        + pn.geom_line(size=1, data=pareto_df)
-        + pn.xlab("Communicative cost of languages")
-        + pn.ylab("Complexity of languages")
-        + pn.scale_color_cmap("cividis")
-    )
-    return plot
--- src/altk/language/language.py
+++ src/altk/language/language.py
@@ -47,10 +47,6 @@ class Language:
         self.expressions = expressions
         self.universe = expressions[0].meaning.universe
 
-        # if "name" in kwargs:
-            # self.name = kwargs["name"]
-        # if "measurements" in kwargs:
-            # self.measurements = kwargs["measurements"]
         if "data" in kwargs:
             self.data = kwargs["data"]
 
