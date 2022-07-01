69d2d3b0e0c1f559d34d8bd85badf186a6efd026
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2022-07-01 12:26:58 -0700

    removed complexity module and refactored informativity to be just functions

--- src/altk/effcomm/informativity.py
+++ src/altk/effcomm/informativity.py
@@ -1,102 +1,14 @@
 """Functions for measuring informativity in efficient communication analyses of languages."""
 
 import numpy as np
-from abc import abstractmethod
 from cmath import isclose
-from typing import Callable, Iterable
+from typing import Callable
 from altk.language.language import Language
 from altk.language.semantics import Meaning, Universe
-from altk.effcomm.agent import *
-
-
-##############################################################################
-# Informativity Classes
-##############################################################################
-
-
-class InformativityMeasure:
-    def __init__(self):
-        raise NotImplementedError
-
-    def batch_communicative_cost(self, langs: list[Language]) -> list[float]:
-        """Measure the (1 - informativity) of a list of languages."""
-        return [1 - self.language_informativity(lang) for lang in langs]
-
-    def batch_informativity(self, langs: list[Language]) -> list[float]:
-        """Measure the informativity of a list of languages."""
-        return [self.language_informativity(lang) for lang in langs]
-
-    @abstractmethod
-    def language_informativity(self, language: Language) -> float:
-        """Measure the informativity of a single language."""
-        raise NotImplementedError
-
-
-class SST_Informativity_Measure(InformativityMeasure):
-
-    """Class for computing the measure of informativeness for a language based on the communicative success of speakers and listeners as described in Steinert-Threlkeld (2021)."""
-
-    def __init__(self, prior: Iterable, utility: np.ndarray, agent_type="literal"):
-        """Initialize the informativity measure with a utility function and prior.
-
-        Args:
-            prior: a probability distribution representing communicative need (frequency) for meanings.
-
-            utility: a 2d numpy array of size |meanings| by |meanings|, containing the function representing the usefulness of listener guesses about speaker meanings, e.g. meaning similarity. For exact recovery, specify an identity matrix.
-
-            kind: Whether to measure informativity using literal or pragmatic agents, as canonically described in the Rational Speech Act framework.
-        """
-        self.prior = prior
-        self.utility = utility
-        self.agent_type = agent_type
-
-    def language_informativity(self, language: Language) -> float:
-        """The informativity of a language is based on the successful communication between a Sender and a Receiver.
-
-        _Concepts_
-            The Sender can be thought of as a conditional distribution over expressions given meanings. The Receiver is likewise a conditional distribution over meanings given expressions. The communicative need, or cognitive source, is a prior probability over meanings representing how frequently agents need to use certain meanings in communication. The utility function represents the similarity, or appropriateness, of the Receiver's guess m' about the Sender's intended meaning m.
-
-        _Formula_
-            The informativity of a language $L$ with meaning space $M$ is defined:
-
-            $I(L) := \sum_{m \in M} p(m) \sum_{i \in L} p(i|m) \sum_{m' \in i} p(m'|i) * u(m, m')$
-
-        _Bounds_
-            A perfectly informative (=1.0) language can be constructed with a exactly one expression for each meaning.
-
-            For u() = indicator(), every language has nonzero informativity because a language must contain at least one expression, and an expression must contain at least one meaning.
-        """
-        if not language.expressions:
-            raise ValueError(f"language empty: {language}")
-
-        speaker = LiteralSpeaker(language)
-        listener = LiteralListener(language)
-
-        if self.agent_type == "literal":
-            pass
-        elif self.agent_type == "pragmatic":
-            speaker = PragmaticSpeaker(language, listener)
-            listener = PragmaticListener(language, speaker, np.diag(self.prior))
-        else:
-            raise ValueError(
-                f"agent_type must be either 'literal' or 'pragmatic'. Received: {self.agent_type}."
-            )
-
-        inf = communicative_success(speaker, listener, self.prior, self.utility)
-
-        # Check informativity > 0
-        m, _ = self.utility.shape  # square matrix
-        if np.array_equal(self.utility, np.eye(m)):
-            if isclose(inf, 0.0):
-                raise ValueError(
-                    f"Informativity must be nonzero for indicator utility reward function, but was: {inf}"
-                )
-
-        return inf
-
+from altk.effcomm.agent import Speaker, Listener, LiteralListener, LiteralSpeaker, PragmaticSpeaker, PragmaticListener
 
 ##############################################################################
-# Main and utility functions for informativity calculation
+# Helper functions for informativity calculation
 ##############################################################################
 
 
@@ -122,6 +34,69 @@ def compute_sparsity(mat: np.ndarray) -> float:
     total = mat.shape[0] * mat.shape[1]
     zeros = np.count_nonzero(mat == 0)
     return float(zeros / total)
+
+##############################################################################
+# Main informativity functions
+##############################################################################
+
+
+def informativity(
+    language: Language,
+    prior: np.ndarray,
+    utility: np.ndarray,
+    agent_type: str = "literal",
+) -> float:
+    """The informativity of a language is based on the successful communication between a Sender and a Receiver.
+
+    Args:
+        language: the language to compute informativity of.
+
+        prior: a probability distribution representing communicative need (frequency) for meanings.
+
+        utility: a 2d numpy array of size |meanings| by |meanings|, containing the function representing the usefulness of listener guesses about speaker meanings, e.g. meaning similarity. To reward only exact recovery of meanings. pass the identity_{|meanings|} matrix.
+
+        kind: {"literal, pragmatic"} Whether to measure informativity using literal or pragmatic agents, as canonically described in the Rational Speech Act framework. The default is "literal".
+
+    _Concepts_
+        The Sender can be thought of as a conditional distribution over expressions given meanings. The Receiver is likewise a conditional distribution over meanings given expressions. The communicative need, or cognitive source, is a prior probability over meanings representing how frequently agents need to use certain meanings in communication. The utility function represents the similarity, or appropriateness, of the Receiver's guess m' about the Sender's intended meaning m.
+
+    _Formula_
+        The informativity of a language $L$ with meaning space $M$ is defined:
+
+        $I(L) := \sum_{m \in M} p(m) \sum_{i \in L} p(i|m) \sum_{m' \in i} p(m'|i) * u(m, m')$
+
+    _Bounds_
+        A perfectly informative (=1.0) language can be constructed with a exactly one expression for each meaning.
+
+        For u() = indicator(), every language has nonzero informativity because a language must contain at least one expression, and an expression must contain at least one meaning.
+    """
+    if not language.expressions:
+        raise ValueError(f"language empty: {language}")
+
+    speaker = LiteralSpeaker(language)
+    listener = LiteralListener(language)
+
+    if agent_type == "literal":
+        pass
+    elif agent_type == "pragmatic":
+        speaker = PragmaticSpeaker(language, listener)
+        listener = PragmaticListener(language, speaker, np.diag(prior))
+    else:
+        raise ValueError(
+            f"agent_type must be either 'literal' or 'pragmatic'. Received: {agent_type}."
+        )
+
+    inf = communicative_success(speaker, listener, prior, utility)
+
+    # Check informativity > 0
+    m, _ = utility.shape  # square matrix
+    if np.array_equal(utility, np.eye(m)):
+        if isclose(inf, 0.0):
+            raise ValueError(
+                f"Informativity must be nonzero for indicator utility reward function, but was: {inf}"
+            )
+
+    return inf
 
 
 def communicative_success(
