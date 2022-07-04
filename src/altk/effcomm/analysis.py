035f0470c622d7e715a70deea61fa7c77be48210
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2022-07-04 16:56:29 -0700

    added to pearson analysis docstring

--- src/altk/effcomm/analysis.py
+++ src/altk/effcomm/analysis.py
@@ -62,6 +62,15 @@ def pearson_analysis(
         property: a string representing a column to measure pearson r with the predictor column
 
         num_bootstrap_samples: how many samples to bootstrap from the original data
+    
+    Returns:
+        a dict of the pearson correlation coefficient for the predictor and the property, and bootstrapped confidence intervals for this coefficient, e.g.
+        {
+            "rho": (a float between -1 and 1),
+            "confidence_intervals": (a pandas Dataframe with the columns [
+                'bootstrap_sample_percent', 'low', 'high'
+            ])
+        }
     """
     min_percent = 0.01  # must be > 2/ len(data)
     intervals = 5
