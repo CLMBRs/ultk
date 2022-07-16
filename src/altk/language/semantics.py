94e98292b3955528109d98fcadb435aec4a93dd0
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2022-07-16 15:44:57 -0700

    new changes from atomic-n-example for to_language

--- src/altk/language/semantics.py
+++ src/altk/language/semantics.py
@@ -1,6 +1,6 @@
-"""Classes for modeling the literal meanings of a language.
+"""Classes for modeling the meanings of a language.
 
-    Meanings are modeled as things which map linguistic forms to objects of reference. The linguistic forms and objects of reference can be richly defined, but they are opaque to the meaning.
+    Meanings are modeled as things which map linguistic forms to objects of reference. The linguistic forms and objects of reference can be richly defined, but they are in some sense inaccessible at the level of a meaning. We think of meanings as informationally encapsulated instructions for building concepts.
 
     In efficient communication analyses, simplicity and informativeness are typically properties of semantic aspects of a language. E.g., a meaning is simple if it is easy to represent, or to compress into some code; a meaning is informative if it is easy for a listener to recover a speaker's intended literal meaning.
 
@@ -18,11 +18,24 @@
 from typing import Any, Iterable
 
 
+class Referent:
+    """A referent is an object of communication."""
+    def __init__(self, name: str, weight: float = None) -> None:
+        self.name = name
+        self.weight = weight
+
+    def __str__(self) -> str:
+        raise NotImplementedError
+
+    def __hash__(self) -> int:
+        raise NotImplementedError
+
+
 class Universe:
 
     """The universe is the set of possible referent objects for a meaning."""
 
-    def __init__(self, objects: Iterable[Any]):
+    def __init__(self, objects: Iterable[Referent]):
         self.objects = objects
 
     def __str__(self):
@@ -50,16 +63,44 @@ class Meaning:
     A meaning is always a subset of the universe, because an expression may itself be underspecified: that is, the expression can be used to express different meanings. Sometimes these different literal meanings are not equally likely, in which it can be helpful to define a meaning as a distribution over objects in the universe.
     """
 
-    def __init__(self, dist: dict[Any, float], universe: Universe) -> None:
-        """A meaning is a probability distribution over a set of possible objects of reference. 
+    def __init__(self, referents: Iterable[Referent], universe: Universe) -> None:
+        """A meaning is the set of objects it refers to.
+
+        The objects of reference are a subset of the universe of discourse. Sometimes it is natural to construe the meaning as as a probability distribution over the universe, instead of just a binary predicate.
         
         Args: 
             dist: a dict with objects of reference as keys, and probabilities as values. The keys must be exactly the objects in `universe`.
 
             universe: a Universe object that defines the probability space for a meaning.
         """
-        if set(dist.keys()) != set(universe.objects): 
-            raise ValueError(f"The keys of `dist` must match the Universe exactly; received keys={dist.keys()}, universe={universe.objects}")
+        if not set(referents).issubset(set(universe.objects)):
+            raise ValueError(f"The set of referents for a meaning must be a subset of the universe of discourse.")
 
-        self.dist = dist
+        self.objects = referents
         self.universe = universe
+        self.dist = None
+
+    def construct_distribution(self, weighted=False):
+        """Construct the probability distributino associated with the meaning.
+
+        By default, all elements in extension are assigned equal probability. If `weighted` set to true, elements are assigned probability according proportional to their weight attribute.
+
+        Args:
+            weighted: a bool representing what weight to assign all elements in the extension of the meaning a probability. 
+        """
+        zeros = {ref.name: 0.0 for ref in set(self.universe.objects) - set(self.objects)}
+        nonzeros = self.weighted_distribution() if weighted else self.referents_uniform()
+        self.dist = nonzeros | zeros
+
+    def referents_uniform(self):
+        """Construct a probability distribution associated with the meaning such that every referent is equally weighted."""
+        return {ref.name: 1/len(self.objects) for ref in self.objects}
+
+    def weighted_distribution(self):
+        """Construct a probability distribution associated with the meaning according to the weights specified by each referent."""
+        total_weight = sum([ref.weight for ref in self.objects.objects])
+        if not total_weight:
+            # If there are no weights, make each referent equally likely.
+            # One might also raise an error, complaining that there must be some nonzero weight in the space of referents.
+            return self.referents_uniform()
+        return {ref.name: ref.weight / total_weight for ref in self.objects}
