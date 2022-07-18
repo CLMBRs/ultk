1647307b3c616e60b26b239d52956d55de318930
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2022-07-18 10:38:07 -0700

    update installation instructions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="altk",
-    version="0.0.1",
+    version="1.0.0",
     author="Nathaniel Imel",
     author_email="nimel@uw.edu",
     description="The Artificial Language ToolKit",
