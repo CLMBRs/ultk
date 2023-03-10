18a309023f5bfd864a96ca4db92907905f4d2f29
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-03-10 14:54:21 -0800

    hashing for sampling

--- src/examples/indefinites/main.py
+++ src/examples/indefinites/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from altk.language.sampling import all_expressions, all_meanings
+from altk.language.language import Language
+from altk.language.sampling import all_expressions, all_meanings, generate_languages
 
 from grammar import indefinites_grammar
 from meaning import universe as indefinites_universe
@@ -15,6 +16,11 @@ if __name__ == "__main__":
         print(exp.to_meaning(indefinites_universe))
     """
 
-    expressions = all_expressions(all_meanings(indefinites_universe))
+    expressions = list(all_expressions(all_meanings(indefinites_universe)))
     for exp in expressions:
         print(exp)
+
+    languages = generate_languages(Language, expressions, 8, 1000)["languages"]
+    print(len(languages))
+    print(languages[0])
+    print(languages[123])
