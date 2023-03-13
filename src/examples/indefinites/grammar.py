d54b0e910dbb9872229bf05d22539c432c0c92c1
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-03-13 13:34:11 -0400

    add negative features to grammar

--- src/examples/indefinites/grammar.py
+++ src/examples/indefinites/grammar.py
@@ -20,8 +20,16 @@ indefinites_grammar.add_rule(Rule("fc", Referent, None, universe["freechoice"]))
 """
 
 # primitive features
+# We include "positive" and "negative" features as primitives (instead of definining the latter via negation) for two reasons.
+# (1) Conceptually, it's not clear that the positive ones are any more basic than the negative ones.  But defining them in
+# terms of negation would make them more complex according to our measure.
+# (2) Computationally, this greatly shrinks the space of grammatical expressions that need to be explored before finding one
+# that expresses each meaning.
 indefinites_grammar.add_rule(
     Rule("K+", bool, None, lambda point: point.name == "specific-known")
+)
+indefinites_grammar.add_rule(
+    Rule("K-", bool, None, lambda point: point.name != "specific-known")
 )
 indefinites_grammar.add_rule(
     Rule(
@@ -33,6 +41,14 @@ indefinites_grammar.add_rule(
 )
 indefinites_grammar.add_rule(
     Rule(
+        "S-",
+        bool,
+        None,
+        lambda point: point.name not in ("specific-known", "specific-unknown"),
+    )
+)
+indefinites_grammar.add_rule(
+    Rule(
         "SE+",
         bool,
         None,
@@ -40,7 +56,18 @@ indefinites_grammar.add_rule(
     )
 )
 indefinites_grammar.add_rule(
+    Rule(
+        "SE-",
+        bool,
+        None,
+        lambda point: point.name not in ("npi", "freechoice", "negative-indefinite"),
+    )
+)
+indefinites_grammar.add_rule(
     Rule("N+", bool, None, lambda point: point.name == "negative-indefinite")
+)
+indefinites_grammar.add_rule(
+    Rule("N-", bool, None, lambda point: point.name != "negative-indefinite")
 )
 indefinites_grammar.add_rule(
     Rule("R+", bool, None, lambda point: point.name in ("negative-indefinite", "npi"))
