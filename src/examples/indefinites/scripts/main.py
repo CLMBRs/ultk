9e0847d31ce39155b71caf940e52c2907278d245
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-03-13 15:27:59 -0400

    generate_expressions.py

--- src/examples/indefinites/scripts/main.py
+++ src/examples/indefinites/scripts/main.py
@@ -1,33 +1,13 @@
 import pandas as pd
 
 from altk.effcomm.informativity import informativity
-from altk.language.language import Expression, Language, aggregate_expression_complexity
+from altk.language.language import aggregate_expression_complexity
 from altk.language.sampling import all_languages, random_languages
-from altk.language.semantics import Meaning
 
 
 from ..grammar import indefinites_grammar
 from ..meaning import universe as indefinites_universe
-
-
-def read_natural_languages(filename: str) -> list[Language]:
-    lang_data = pd.read_csv(filename)
-    lang_data["flavors"] = lang_data.apply(
-        lambda row: row[row == True].index.tolist(), axis=1
-    )
-    language_frame = lang_data.groupby("language")
-    languages = set()
-    for lang, items in language_frame:
-        cur_expressions = []
-        for item in items.itertuples():
-            cur_meaning = Meaning(
-                [indefinites_universe[flavor] for flavor in item.flavors],
-                indefinites_universe,
-            )
-            cur_expressions.append(Expression(item.expression, cur_meaning))
-        languages.add(Language(cur_expressions, name=lang, natural=True))
-    return languages
-
+from ..util import read_natural_languages
 
 if __name__ == "__main__":
 
@@ -44,12 +24,7 @@ if __name__ == "__main__":
 
     expressions = list(meanings_by_expressions.values())
 
-    languages = list(all_languages(expressions, max_size=3))
-    print(len(languages))
-
-    languages = list(random_languages(expressions, 1000, max_size=8))
-    print(len(languages))
-    print([len(language) for language in languages])
+    seed_languages = list(random_languages(expressions, 1000, max_size=8))
 
     language = languages[56]
     print(language)
@@ -59,12 +34,5 @@ if __name__ == "__main__":
         return aggregate_expression_complexity(
             # TODO: change this measure to closer match the paper?
             language,
-            lambda gram_expr: len(gram_expr),
+            lambda expression: len(meanings_by_expressions[expression.meaning]),
         )
-
-    print(complexity(language))
-
-    for language in read_natural_languages("indefinites/data/natural_language_indefinites.csv"):
-        print(language)
-        print(language.name)
-        print(language.natural)
