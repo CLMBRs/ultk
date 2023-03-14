5132965e1801f8e8461f9f0966da012505b628e4
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-03-14 10:55:41 -0400

    measure natural languages!

--- src/examples/indefinites/scripts/measure_natural_languages.py
+++ src/examples/indefinites/scripts/measure_natural_languages.py
@@ -1,10 +1,6 @@
-import pandas as pd
-
-from altk.language.semantics import Meaning
-
-from ..grammar import indefinites_grammar
 from ..meaning import universe as indefinites_universe
-from ..util import read_expressions, read_natural_languages
+from ..measures import comm_cost, complexity
+from ..util import read_expressions, read_natural_languages, write_languages
 
 if __name__ == "__main__":
     natural_languages = read_natural_languages(
@@ -18,6 +14,18 @@ if __name__ == "__main__":
     assert all(
         expression.meaning in expressions_by_meaning
         for language in natural_languages
-        for expression in language
+        for expression in language.expressions
     )
 
+    write_languages(
+        natural_languages,
+        "indefinites/outputs/natural_languages.yml",
+        {
+            "name": lambda _, lang: lang.name,
+            "lot_expressions": lambda _, lang: [
+                str(expressions_by_meaning[expr.meaning]) for expr in lang.expressions
+            ],
+            "complexity": lambda _, lang: complexity(lang, expressions_by_meaning),
+            "comm_cost": lambda _, lang: comm_cost(lang),
+        },
+    )
