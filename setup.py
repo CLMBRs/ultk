e6f38f7e2d31d3d470785c8bb314296f6cab2400
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-03-14 11:18:18 -0400

    version 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="altk",
-    version="1.0.0",
+    version="0.0.1",
     author="Nathaniel Imel, Shane Steinert-Threlkeld",
     author_email="nimel@uci.edu, shanest@uw.edu",
     description="The Artificial Language ToolKit",
