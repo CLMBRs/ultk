3126e6aeb780912883f8e34ebc6a5c2f51ed06cb
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-03-15 11:08:44 -0400

    some notes and cleaning of indefinites grammar

--- src/examples/indefinites/grammar.py
+++ src/examples/indefinites/grammar.py
@@ -1,6 +1,4 @@
-from typing import Callable
 from altk.language.grammar import Grammar, Rule
-from altk.language.semantics import Referent
 
 
 indefinites_grammar = Grammar(bool)
@@ -9,22 +7,13 @@ indefinites_grammar.add_rule(Rule("and", bool, (bool, bool), lambda p1, p2: p1 a
 indefinites_grammar.add_rule(Rule("or", bool, (bool, bool), lambda p1, p2: p1 or p2))
 indefinites_grammar.add_rule(Rule("not", bool, (bool,), lambda p1: not p1))
 
-"""
-from meaning import universe
-
-Feature = Callable[[Referent], bool]
-
-indefinites_grammar.add_rule(Rule("apply", bool, (Feature, Referent), lambda feat, ref: feat(ref)))
-indefinites_grammar.add_rule(Rule("K+", Feature, None, lambda ref: ref.name == "specific-known"))
-indefinites_grammar.add_rule(Rule("fc", Referent, None, universe["freechoice"]))
-"""
 
 # primitive features
 # We include "positive" and "negative" features as primitives (instead of definining the latter via negation) for two reasons.
 # (1) Conceptually, it's not clear that the positive ones are any more basic than the negative ones.  But defining them in
 # terms of negation would make them more complex according to our measure.
 # (2) Computationally, this greatly shrinks the space of grammatical expressions that need to be explored before finding one
-# that expresses each meaning.
+# that expresses each meaning.  Better search and/or minimization algorithms would help here.
 indefinites_grammar.add_rule(
     Rule("K+", bool, None, lambda point: point.name == "specific-known")
 )
@@ -69,6 +58,9 @@ indefinites_grammar.add_rule(
 indefinites_grammar.add_rule(
     Rule("N-", bool, None, lambda point: point.name != "negative-indefinite")
 )
+# NB: the grammar should be modified in such a way that R+ and R- can only occur with SE+
+# easiest would be to just split SE+ into two features
+# more elegant: extra grammar rule (will preserve the impact on complexity)
 indefinites_grammar.add_rule(
     Rule("R+", bool, None, lambda point: point.name in ("negative-indefinite", "npi"))
 )
