a9a37db005b8f3ff6eb5d58f6aa9e7e001ca0cdf
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-03-16 12:01:43 -0400

    some docstring in util

--- src/examples/indefinites/util.py
+++ src/examples/indefinites/util.py
@@ -17,20 +17,37 @@ from .meaning import universe as indefinites_universe
 
 
 def read_natural_languages(filename: str) -> list[Language]:
+    """Read the natural languages from a CSV file.
+    Assumes that each row is one expression, with unique strings in "language" column identifying
+    which expressions belong to which languages.
+    Assumes that there is a boolean-valued column for each Referent in the indefinites_universe,
+    identified by its name.
+
+    Args:
+        filename: the file to read
+
+    Returns:
+        a list of Languages
+    """
     lang_data = pd.read_csv(filename)
     lang_data["flavors"] = lang_data.apply(
         lambda row: row[row == True].index.tolist(), axis=1
     )
+    # group data frame by language
     language_frame = lang_data.groupby("language")
     languages = set()
+    # iterate through each language
     for lang, items in language_frame:
         cur_expressions = []
         for item in items.itertuples():
+            # generate Meaning from list of flavors
             cur_meaning = Meaning(
                 [indefinites_universe[flavor] for flavor in item.flavors],
                 indefinites_universe,
             )
+            # add Expression with form and Meaning
             cur_expressions.append(Expression(item.expression, cur_meaning))
+        # add Language with its Expressions
         languages.add(Language(cur_expressions, name=lang, natural=True))
     return languages
 
@@ -38,6 +55,11 @@ def read_natural_languages(filename: str) -> list[Language]:
 def read_expressions(
     filename: str, universe: Universe = None, return_by_meaning=True
 ) -> tuple[list[GrammaticalExpression], dict[Meaning, Expression]]:
+    """Read expressions from a YAML file.
+    Assumes that the file is a list, and that each item in the list has a field
+    "grammatical_expression" with an expression that can be parsed by the 
+    indefinites_grammar.
+    """
     with open(filename, "r") as f:
         expression_list = load(f, Loader=Loader)
     parsed_exprs = [
