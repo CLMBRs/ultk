b3924c5e3e7037de60a100f46f85e407741edf56
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-04-10 09:34:52 -0700

    add is_atom and docstrings

--- src/altk/language/grammar/complexity.py
+++ src/altk/language/grammar/complexity.py
@@ -2,10 +2,37 @@ from altk.language.grammar.grammar import GrammaticalExpression
 
 
 def expression_length(expr: GrammaticalExpression) -> int:
+    """Get the expression length (number of nodes in a tree).
+    See `GrammaticalExpression.__len__` for more information.
+
+    Args:
+        expr: the expression
+
+    Returns:
+        the expression's length
+    """
     return len(expr)
 
 
 def num_atoms(expr: GrammaticalExpression) -> int:
-    if expr.children is None:
+    """Count the number of atoms in a GrammaticalExpression.
+
+    Args:
+        expr: the expression
+
+    Returns:
+        the number of atoms
+    """
+    if expr.is_atom():
         return 1
     return sum(num_atoms(child) for child in expr.children)
+
+
+################################################################
+# Boolean Operations
+################################################################
+
+
+def distribute_and(expr: GrammaticalExpression) -> GrammaticalExpression:
+    """(p and q) or (p and s) -> p and (q or s)"""
+    pass
