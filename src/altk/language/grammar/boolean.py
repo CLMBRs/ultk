705cc661245e15514371541545a018465f2c115d
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-04-21 15:15:23 -0700

    rule_names --> enum :)

--- src/altk/language/grammar/boolean.py
+++ src/altk/language/grammar/boolean.py
@@ -1,18 +1,22 @@
-from types import SimpleNamespace
+from enum import StrEnum
 from typing import Callable
 from altk.language.grammar.grammar import Grammar, GrammaticalExpression, Rule
 
-rule_names = SimpleNamespace(AND="and", OR="or", NOT="not")
+
+class RuleNames(StrEnum):
+    AND = "and"
+    OR = "or"
+    NOT = "not"
 
 
 class BooleanGrammar(Grammar):
     def __init__(self):
         super().__init__(bool)
         self.add_rule(
-            Rule(rule_names.AND, bool, (bool, bool), lambda p1, p2: p1 and p2)
+            Rule(RuleNames.AND, bool, (bool, bool), lambda p1, p2: p1 and p2)
         )
-        self.add_rule(Rule(rule_names.OR, bool, (bool, bool), lambda p1, p2: p1 or p2))
-        self.add_rule(Rule(rule_names.NOT, bool, (bool,), lambda p1: not p1))
+        self.add_rule(Rule(RuleNames.OR, bool, (bool, bool), lambda p1, p2: p1 or p2))
+        self.add_rule(Rule(RuleNames.NOT, bool, (bool,), lambda p1: not p1))
 
     def add_atom(self, name: str, function: Callable) -> None:
         self.add_rule(Rule(name, bool, None, function))
