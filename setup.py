54c3b3b2345e4e84fefe4eea3b4a715ff485a703
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2023-06-01 11:07:59 -0700

    full support for pyproject.toml

--- setup.py
+++ setup.py
@@ -1,34 +1,4 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="clmbr-altk",
-    version="0.0.1a",
-    author="Nathaniel Imel, Shane Steinert-Threlkeld",
-    author_email="nimel@uci.edu, shanest@uw.edu",
-    description="The Artificial Language ToolKit",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/CLMBRs/altk",
-    project_urls={"Bug Tracker": "https://github.com/CLMBRs/altk/issues"},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
-    install_requires=[
-        "numpy",
-        "nltk",
-        "pyyaml",
-        "pandas",
-        "plotnine",
-        "pathos",
-        "embo",
-        "pytest",
-    ],
-)
+if __name__ == '__main__':
+    setuptools.setup()
