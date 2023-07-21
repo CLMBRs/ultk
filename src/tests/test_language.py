e195450f6dabfc74a69efb7c523ec2d7cfd3009f
mickeyshi <mickeyshi96@yahoo.com>
2023-07-21 11:44:41 -0700

    Made POS changes to tests

--- src/tests/test_language.py
+++ src/tests/test_language.py
@@ -6,29 +6,37 @@ from ultk.language.language import Expression, Language
 from ultk.language.semantics import Referent, Universe, Meaning
 
 class TestLanguage:
-    pairs = {"the":"det", "dog":"noun", "jumps":"verb", "cat":"noun"}
-    pairs2 = {"the":"det", "dog":"noun", "jumps":"verb", "bird":"noun"}
+    pairs = {"shroom":"fungus", "dog":"animal", "tree":"plant", "cat":"animal"}
+    pairs2 = {"shroom":"fungus", "dog":"animal", "tree":"plant", "bird":"bird"}
 
-    uni_refs = [Referent(key, {"pos": val}) for (key,val) in pairs.items()]
+    uni_refs = [Referent(key, {"phylum": val}) for (key,val) in pairs.items()]
     uni = Universe(uni_refs)
-    uni2 = Universe([Referent(key, {"pos": val}) for (key,val) in pairs2.items()])
+    uni2 = Universe([Referent(key, {"phylum": val}) for (key,val) in pairs2.items()])
 
     meaning = Meaning(referents=uni_refs, universe=uni)
-    exp = Expression(form="dog", meaning=Meaning(referents=[Referent("dog", {"pos":"noun"})], universe=uni))
-    exp2 = Expression(form="cat", meaning=Meaning(referents=[Referent("cat", {"pos":"noun"})], universe=uni))
-    exp3 = Expression(form="jumps", meaning=Meaning(referents=[Referent("jumps", {"pos":"verb"})], universe=uni))
+    exp = Expression(form="dog", meaning=Meaning(referents=[Referent("dog", {"phylum":"animal"})], universe=uni))
+    exp2 = Expression(form="cat", meaning=Meaning(referents=[Referent("cat", {"phylum":"animal"})], universe=uni))
+    exp3 = Expression(form="tree", meaning=Meaning(referents=[Referent("tree", {"phylum":"plant"})], universe=uni))
+    exp4 = Expression(form="tree", meaning=Meaning(referents=[Referent("shroom", {"phylum":"fungus"})], universe=uni))
 
-    lang = Language(expressions=[exp, exp2])
+    lang = Language(expressions=[exp, exp2, exp3, exp4])
 
     def test_exp_subset(self):
-        assert TestLanguage.exp.can_express(Referent("dog", {"pos":"noun"}))
+        assert TestLanguage.exp.can_express(Referent("dog", {"phylum":"animal"}))
     def test_exp_subset(self):
-        assert not TestLanguage.exp.can_express(Referent("cat", {"pos":"noun"}))
+        assert not TestLanguage.exp.can_express(Referent("cat", {"phylum":"animal"}))
     def test_language_has_expressions(self):
         with pytest.raises(ValueError):
             lang2 = Language([])
     def test_language_universe_check(self):
         with pytest.raises(ValueError):
             lang2 = Language([TestLanguage.exp, 
-                              Expression(form="dog", meaning=Meaning(referents=[Referent("dog", {"pos":"noun"})], universe=TestLanguage.uni2))])
- 
+                              Expression(form="dog", meaning=Meaning(referents=[Referent("dog", {"phylum":"animal"})], universe=TestLanguage.uni2))])
+    def test_language_degree(self):
+        def isAnimal(exp:Expression) -> bool:
+            print("checking phylum of " + str(exp.meaning.referents[0]))
+            return exp.meaning.referents[0].to_dict()["phylum"] == "animal"
+        assert TestLanguage.lang.degree_property(isAnimal) == .5
+
+    def test_language_len(self):
+        assert TestLanguage.lang.__len__() == 4
