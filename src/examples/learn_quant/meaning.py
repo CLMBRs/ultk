d7b6e6e795a0de482daa63b5abea105853b026c3
haberchr <haberc@uw.edu>
2023-11-09 15:13:42 -0800

    Ensure elements of M may be any index in alien set in defined range(0, X)

--- src/examples/learn_quant/meaning.py
+++ src/examples/learn_quant/meaning.py
@@ -1,36 +1,29 @@
 import pandas as pd
 from altk.language.semantics import Universe
 from learn_quant.quantifier import QuantifierModel
-from itertools import product
+from itertools import product, combinations, permutations
+from altk.language.sampling import powerset
 import random
 
-M_SIZE = 1
-referent_pertinence = pd.read_csv("learn_quant/index.csv").to_dict()
-#referent_pertinence = {"value": ["A", "B", "both", "neither"]}
+M_SIZE = 4
+X_SIZE = 6
 
-def create_universe(referent_pertinence, M_SIZE):
 
-    quantifier_names = []
-    for size in range(M_SIZE+1):
-        index = [i for i in range(len(referent_pertinence["value"]))]
+referent_pertinence = {"value": ["A", "B", "both", "neither"]}
 
-        x_universe = product("".join([str(x) for x in index]), repeat=size)
+def create_universe(referent_pertinence, M_SIZE, X_SIZE):
+    quantifiers_list = []
+    for m_size in range(M_SIZE):
+        quantifiers_at_msize = []
+        possible_M = [x for x in combinations(range(X_SIZE), M_SIZE)]
+        for M in possible_M:
+            possible_AorB = [set(x) for x in powerset(M)]
+            combs_A_B_for_M = [z for z in product(possible_AorB, possible_AorB)]
+            for (A, B) in combs_A_B_for_M:
+                quantifiers_at_msize.append([set(M), A, B])
+        quantifiers_list.extend(quantifiers_at_msize)
+    quantifier_models_list = [QuantifierModel(M=m, A=a, B=b, X=X_SIZE) for (m, a, b) in quantifiers_list]
+    return quantifier_models_list
 
-        quantifier_names.extend(["".join(array) for array in x_universe])
-
-        quantifier_list = [QuantifierModel(name) for name in quantifier_names if name != '']
-
-    return Universe(quantifier_list)
-
-quantifiers_universe = create_universe(referent_pertinence, M_SIZE)
-
-"""
-
-print(len(quantifiers_universe))
-
-set([x for x in quantifiers_universe.referents])
-
-set([sorted({2,3,6,4,7})[3]])
-set(6)
-set(sorted({2,3,6,4,7})[3])
-"""
+quantifier_universe = create_universe(referent_pertinence, M_SIZE, X_SIZE)
+print("The size of the universe is {}".format(len(quantifier_universe)))
--- src/examples/learn_quant/quantifier.py
+++ src/examples/learn_quant/quantifier.py
@@ -7,17 +7,25 @@ from dataclasses import dataclass
 
 @dataclass(eq=False)
 class QuantifierModel(Referent):
-    """A quantifier model is a single referent that captures a particular interpretation of a quantifier meaning, which is a set of quantifier referents."""
+    """A quantifier model is a single referent that captures a particular interpretation of a quantifier meaning, which is a set of quantifier referents.
+
+    Every quantifier model is a quadruple <M, A, B, X>, where M corresponds to all possible quantifier referents for a given communicative situation, A and B are differents sets of quantifier referents that correspond to the items of comparison in quantificational logic, and X corresponds to the space of all possible referents that are a superset or equal to the referents in M. That is, X is conceptually the set of possible referents in any quantification expression modeled in the universe.
+
+    A scalar index (single referent in a set M, A, B) from [0, X) corresponds to a unique referent in a quantifier model.
+
+    X is conceptually a set but is listed as an integer that denotes the length of an array [0, X).
+    """
 
     name: str = None
     M: set = None
     A: set = None
     B: set = None
+    X: int = None
 
     def __post_init__(self):
 
         if not self.name and not (self.M and self.A and self.B):
-            raise ValueError("Must initialize with either a name or sets for <M, A, B>")
+            raise ValueError("Must initialize with either a 'name' of length X or sets for <M, A, B> and an int value for X")
         
         if self.M and self.A and self.B:
             self._update_name()
@@ -29,53 +37,66 @@ class QuantifierModel(Referent):
         if name:
             self.name = name
 
+        self.X = len(name)
+
         self.M = set() 
         self.A = set()
         self.B = set()
         for index, i in enumerate(self.name):
-            self.M.add(int(index))
             if self.name[index] == "0":
                 self.A.add(int(index))
+                self.M.add(int(index))
             if self.name[index] == "1":
                 self.B.add(int(index))
+                self.M.add(int(index))
             if self.name[index] == "2":
                 self.A.add(int(index))
                 self.B.add(int(index))
+                self.M.add(int(index))
+            if self.name[index] == "3":
+                self.M.add(int(index))
                 
-    def _update_name(self, M=None, A=None, B=None):
+    def _update_name(self, **kwargs):
 
-        if M:
-            self.M = M
-        if A:
-            self.A = A
-        if B:
-            self.B = B 
+        allowed_keys = {"M", "A", "B", "X"}
+        self.__dict__.update((k, v) for k, v in kwargs.items() if k in allowed_keys)
 
         name_seq = []
-        for i in sorted(self.M):
-            i_in_both = i in self.A and i in self.B
-            i_in_A = i in self.A and not i_in_both
-            i_in_B = i in self.B and not i_in_both
-            i_in_neither = (not i_in_A) and (not i_in_B)
-            if i_in_A:
+        for i in range(0, self.X):
+            in_both = i in self.A and i in self.B
+            in_A = i in self.A and not in_both
+            in_B = i in self.B and not in_both
+            in_M = i in self.M
+            in_neither = (not in_A) and (not in_B) and (not in_both)
+
+            counter = 0
+            if in_A:
                 name_seq.append("0")
-            if i_in_B:
+            if in_B:
                 name_seq.append("1")
-            if i_in_both:
+            if in_both:
                 name_seq.append("2")
-            if i_in_neither:
+            if in_neither and in_M:
                 name_seq.append("3")
+            if in_neither and not in_M:
+                name_seq.append("4")
+
+        assert len(name_seq) == self.X
+            
         self.name = "".join(name_seq)
 
     def update(self, **kwargs):
 
         self.__dict__.update((key, kwargs[key])
-            for key in ('name', 'M', 'A', 'B') if key in kwargs)
+            for key in ('name', 'M', 'A', 'B', "X") if key in kwargs)
         self._update_name()
         self._update_sets()
 
     def get_cardinalities(self) -> dict:
-        return {"A": len(self.A), "B": len(self.B), "M": len(self.M)}
+        return {"M": len(self.M), 
+                "A": len(self.A), 
+                "B": len(self.B), 
+                "X": self.X}
     
 """
 qm = QuantifierModel(M=set([1,2,3,4]), A=set([1,4]), B=set([2,3]))
