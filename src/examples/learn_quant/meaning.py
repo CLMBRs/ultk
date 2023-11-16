72df8b0e6837dd040272817852bd4f1557103be9
haberchr <haberc@uw.edu>
2023-11-15 18:12:35 -0800

    Converted sets to frozensets, set eq=True frozen=True for QuantifierModel dataclass args, successfully generated output

--- src/examples/learn_quant/meaning.py
+++ src/examples/learn_quant/meaning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from altk.language.semantics import Universe
 from learn_quant.quantifier import QuantifierModel
-from itertools import product, combinations, permutations
+from itertools import product, combinations_with_replacement, permutations
 from altk.language.sampling import powerset
 import random
 
@@ -14,24 +14,42 @@ import random
 """
 
 def create_universe(M_SIZE, X_SIZE):
-    quantifiers_list = []
-    for m_size in range(M_SIZE):
-        quantifiers_at_msize = []
-        possible_M = [x for x in combinations(range(X_SIZE), M_SIZE)]
-        for M in possible_M:
-            possible_AorB = [set(x) for x in powerset(M)]
-            combs_A_B_for_M = [z for z in product(possible_AorB, possible_AorB)]
-            for (A, B) in combs_A_B_for_M:
-                quantifiers_at_msize.append([set(M), A, B])
-        quantifiers_list.extend(quantifiers_at_msize)
-    quantifier_universe = Universe([QuantifierModel(M=m, A=a, B=b) for (m, a, b) in quantifiers_list])
-    return quantifier_universe
+
+    possible_quantifiers = []
+
+    for x in combinations_with_replacement([0,1,2,3], r=M_SIZE):
+        combo = list(x) + [4] * (X_SIZE-M_SIZE)
+        permutations_for_combo = set(permutations(combo, r=len(combo)))
+        possible_quantifiers.extend(permutations_for_combo)
+    possible_quantifiers_name = set(["".join([str(j) for j in i]) for i in possible_quantifiers])
+
+    quantifier_models = set()
+    for name in possible_quantifiers_name:
+        M = []
+        A = []
+        B = []
+        for index, i in enumerate(name):
+            if name[index] == "0":
+                A.append(int(index))
+                M.append(int(index))
+            if name[index] == "1":
+                B.append(int(index))
+                M.append(int(index))
+            if name[index] == "2":
+                A.append(int(index))
+                B.append(int(index))
+                M.append(int(index))
+            if name[index] == "3":
+                M.append(int(index))
+        quantifier_models.add(QuantifierModel(name=name, M=frozenset(M), A=frozenset(A), B=frozenset(B)))
+    return Universe(quantifier_models)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Generate expressions')
     parser.add_argument('--m_size', type=int, default=8, help='maximum size of the universe')
     parser.add_argument('--x_size', type=int, default=8, help='number of unique referents from which M may be comprised')
+
     args = parser.parse_args()
 
     quantifier_universe = create_universe(M_SIZE, X_SIZE)
--- src/examples/learn_quant/quantifier.py
+++ src/examples/learn_quant/quantifier.py
@@ -5,7 +5,7 @@ import pandas as pd
 from altk.language.semantics import Referent
 from dataclasses import dataclass
 
-@dataclass(eq=False)
+@dataclass(eq=True, frozen=True)
 class QuantifierModel(Referent):
     """A quantifier model is a single referent that captures a particular interpretation of a quantifier meaning, which is a set of quantifier referents.
 
@@ -13,94 +13,12 @@ class QuantifierModel(Referent):
     """
 
     name: str = None
-    M: set = None
-    A: set = None
-    B: set = None
-
-    def __post_init__(self):
-
-        if not self.name and not (self.M and self.A and self.B):
-            raise ValueError("Must initialize with either a name or sets for <M, A, B>")
-        
-        if self.M and self.A and self.B:
-            self._update_name()
-        elif self.name:
-            self._update_sets()
-    
-    def _update_sets(self, name=None):
-
-        if name:
-            self.name = name
-
-        self.M = set() 
-        self.A = set()
-        self.B = set()
-        for index, i in enumerate(self.name):
-            if self.name[index] == "0":
-                self.A.add(int(index))
-                self.M.add(int(index))
-            if self.name[index] == "1":
-                self.B.add(int(index))
-                self.M.add(int(index))
-            if self.name[index] == "2":
-                self.A.add(int(index))
-                self.B.add(int(index))
-                self.M.add(int(index))
-            if self.name[index] == "3":
-                self.M.add(int(index))
-                
-    def _update_name(self, **kwargs):
-
-        allowed_keys = {"M", "A", "B"}
-        self.__dict__.update((k, v) for k, v in kwargs.items() if k in allowed_keys)
-
-        max_index = max(self.M | self.A | self.B)
-
-        name_seq = []
-        for i in range(0, max_index + 1):
-            in_both = i in self.A and i in self.B
-            in_A = i in self.A and not in_both
-            in_B = i in self.B and not in_both
-            in_M = i in self.M
-            in_neither = (not in_A) and (not in_B) and (not in_both)
-
-            counter = 0
-            if in_A:
-                name_seq.append("0")
-            if in_B:
-                name_seq.append("1")
-            if in_both:
-                name_seq.append("2")
-            if in_neither and in_M:
-                name_seq.append("3")
-            if in_neither and not in_M:
-                name_seq.append("4")
-            
-        self.name = "".join(name_seq)
-
-    def update(self, **kwargs):
-
-        self.__dict__.update((key, kwargs[key])
-            for key in ('name', 'M', 'A', 'B') if key in kwargs)
-        self._update_name()
-        self._update_sets()
+    M: frozenset = None
+    A: frozenset = None
+    B: frozenset = None
 
     def get_cardinalities(self) -> dict:
         return {"M": len(self.M), 
                 "A": len(self.A), 
                 "B": len(self.B), 
                 }
-
-"""   
-qm = QuantifierModel(M=set([1,2,3,4]), A=set([1,4]), B=set([2,3]))
-qm.name
-
-qm2 = QuantifierModel("0030")
-qm2.A = None
-qm2.A
-qm2._update_sets("1111")
-qm2._update_sets("0011")
-
-qm3 = QuantifierModel("13022")
-qm3
-"""
