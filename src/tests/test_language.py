be571100a4aefc359b1e67eb014e48655bc2ca52
haberchr <haberc@uw.edu>
2023-11-29 21:40:25 -0800

    Incorporate Shane's comments

--- src/tests/test_language.py
+++ src/tests/test_language.py
@@ -5,7 +5,6 @@ import pytest
 from ultk.language.language import Expression, Language
 from ultk.language.semantics import Referent, Universe, Meaning
 
-
 class TestLanguage:
     pairs = {
         "shroom": "fungus",
@@ -14,64 +13,69 @@ class TestLanguage:
         "cat": "animal",
         "bird": "animal",
     }
-    pairs2 = {"shroom": "fungus", "dog": "animal", "tree": "plant", "bird": "bird"}
+    pairs2 = {"shroom": "fungus", 
+              "dog": "animal",
+              "tree": "plant",
+              "bird": "bird"}
 
     uni_refs = [Referent(key, {"phylum": val}) for (key, val) in pairs.items()]
     uni = Universe(uni_refs)
+
     uni2 = Universe([Referent(key, {"phylum": val}) for (key, val) in pairs2.items()])
 
     meaning = Meaning(referents=uni_refs, universe=uni)
-    exp = Expression(
+
+    dog = Expression(
         form="dog",
         meaning=Meaning(
             referents=[Referent("dog", {"phylum": "animal"})], universe=uni
         ),
     )
-    exp2 = Expression(
+    cat = Expression(
         form="cat",
         meaning=Meaning(
             referents=[Referent("cat", {"phylum": "animal"})], universe=uni
         ),
     )
-    exp3 = Expression(
+    tree = Expression(
         form="tree",
         meaning=Meaning(
             referents=[Referent("tree", {"phylum": "plant"})], universe=uni
         ),
     )
-    exp4 = Expression(
+    shroom = Expression(
         form="shroom",
         meaning=Meaning(
             referents=[Referent("shroom", {"phylum": "fungus"})], universe=uni
         ),
     )
-    exp5 = Expression(
+    bird = Expression(
         form="bird",
         meaning=Meaning(
             referents=[Referent("bird", {"phylum": "animal"})], universe=uni
         ),
     )
 
-    lang = Language(expressions=[exp, exp2, exp3, exp4])
-    lang2 = Language(expressions=[exp, exp2, exp3, exp5])
-    lang3 = Language(expressions=[exp, exp2, exp3])
-    lang4 = Language(expressions=[exp, exp2, exp4, exp3])
+    lang = Language(expressions=[dog, cat, tree, shroom])
+    lang_one_different_expr = Language(expressions=[dog, cat, tree, bird])
+    lang_subset_expr = Language(expressions=[dog, cat, tree])
+    lang_of_different_order = Language(expressions=[dog, cat, shroom, tree])
 
     def test_exp_subset(self):
-        assert TestLanguage.exp.can_express(Referent("dog", {"phylum": "animal"}))
+        assert TestLanguage.dog.can_express(Referent("dog", {"phylum": "animal"}))
 
     def test_exp_subset(self):
-        assert not TestLanguage.exp.can_express(Referent("cat", {"phylum": "animal"}))
+        assert not TestLanguage.dog.can_express(Referent("cat", {"phylum": "animal"}))
 
     def test_language_has_expressions(self):
         with pytest.raises(ValueError):
-            lang2 = Language([])
+            lang = Language(list())
 
     def test_language_universe_check(self):
         with pytest.raises(ValueError):
-            lang2 = Language(
+            lang_one_different_expr = Language(
                 [
-                    TestLanguage.exp,
+                    TestLanguage.dog,
                     Expression(
                         form="dog",
                         meaning=Meaning(
@@ -85,14 +89,16 @@ class TestLanguage:
     def test_language_degree(self):
         def isAnimal(exp: Expression) -> bool:
             print("checking phylum of " + str(exp.meaning.referents[0]))
-            return exp.meaning.referents[0].to_dict()["phylum"] == "animal"
+            return exp.meaning.referents[0].phylum == "animal"
 
         assert TestLanguage.lang.degree_property(isAnimal) == 0.5
 
     def test_language_len(self):
-        assert TestLanguage.lang.__len__() == 4
+        assert len(TestLanguage.lang) == 4
 
     def test_language_equality(self):
-        assert TestLanguage.lang != TestLanguage.lang2
-        assert TestLanguage.lang != TestLanguage.lang3
-        assert TestLanguage.lang == TestLanguage.lang4
+        assert TestLanguage.lang != TestLanguage.lang_one_different_expr
+        assert TestLanguage.lang != TestLanguage.lang_subset_expr
+
+        # Test that order doesn't matter
+        assert TestLanguage.lang == TestLanguage.lang_of_different_order
