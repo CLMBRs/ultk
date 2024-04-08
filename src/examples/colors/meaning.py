e4b26ad2cf0d7dfc3e2f8547545080c370318e5d
mickeyshi <mickeyshi96@yahoo.com>
2024-04-08 04:01:12 -0700

    Added color term expansion to artificial languages, some cleanup

--- src/examples/colors/meaning.py
+++ src/examples/colors/meaning.py
@@ -24,16 +24,26 @@ from zipfile import ZipFile
 wcs_dialect = csv.Dialect
 wcs_dialect.delimiter = "\t"
 
-language_codes = dict()
+language_name_from_code = dict()
 language_terms = dict()
 
-GENERATE_IB_BOUND=True
-USE_RKK = False
-USE_NOGA_ARRAYS = False
-GENERATE_LANG_COLOR_INFO=False
+GENERATE_IB_BOUND=True #True to generate the IB bound for the specified parameters
+USE_RKK = False #Whether to use the RKK metric for complexity
+USE_NOGA_ARRAYS = False #True to use the Zaslavsky data for the IB bound, false otherwise
+GENERATE_LANG_COLOR_INFO=False #True to generate color information for each language
+GENERATE_ADDITIONAL_COLOR_CHIPS = True #If true, will expand color terms based off color distance for both natural and artificial languages
+USE_ONE_LANGUAGE = False #If true, just uses the first language in the list of languages
 
 #Get current dir for relative paths
 current_dir = os.path.dirname(os.path.realpath(__file__))
+
+#Get the expression from a language based on the color name
+def get_expression_from_language(color_name:str, language:Language):
+    for expression in language.expressions:
+        for referent in expression.meaning.referents:
+            if referent.name == color_name:
+                return expression
+    return None
 
 if(USE_NOGA_ARRAYS):
 
@@ -86,7 +96,7 @@ color_universe = Universe(referents=referents_by_color_code.values())
 with open(f"{current_dir}/data/lang.txt", newline="", encoding="utf-8") as csvfile:
     lang_reader = csv.DictReader(csvfile, delimiter="\t")
     for row in lang_reader:
-        language_codes[row["LNUM"]] = row["LNAME"]
+        language_name_from_code[row["LNUM"]] = row["LNAME"]
 
 #Collect language information, such as term names, abbreviations, etc
 with open(f'{current_dir}/data/dict.txt', newline='', encoding="utf-8" ) as csvfile:
@@ -185,6 +195,15 @@ for language_code in average_language_by_meaning:
         if most_frequent_color_term not in color_names:
             color_names[most_frequent_color_term] = []
         color_names[most_frequent_color_term].append(color)
+
+    #Fill in additional color chips
+    for additional_color in color_universe.referents:
+        if additional_color not in average_language_by_meaning[language_code]:
+            #Get the closest color to the current color
+            closest_color_with_term = min([color_term_ref for color_term_ref in color_universe.referents if color_term_ref.name in average_language_by_meaning[language_code].keys()],
+                                            key=lambda x: np.linalg.norm(np.array((x.L, x.a, x.b)) - np.array((additional_color.L, additional_color.a, additional_color.b))))
+            color_names[average_language_by_meaning[language_code][closest_color_with_term.name].most_common(1)[0][0]].append(additional_color.name)
+
     #Create list of expressions to add to the Language
     language_expressions = []
     for color_name in color_names:
@@ -193,7 +212,7 @@ for language_code in average_language_by_meaning:
         expression_set.add(expression)
         language_expressions.append(expression)
 
-    languages[language_code] = Language(language_expressions, lang_code=language_code)
+    languages[language_code] = Language(language_expressions, lang_code=language_code, name=language_name_from_code[language_code])
 
 #Generate the imshow heatmap for the meaning
 plt.imshow(meaning_dists,  cmap="hot")
@@ -211,15 +230,20 @@ if(USE_NOGA_ARRAYS):
     plt.savefig(f"{current_dir}/outputs/noga_meaning_dists.jpg")
 
 #result = meaning(munsell_to_cielab[meaning_space_indices[0]], munsell_to_cielab[meaning_space_indices[1]])
+if(USE_ONE_LANGUAGE): 
+    first_key = list(languages.keys())[0]
+    languages = {first_key: languages[first_key]}
+    print(f"Using one language: {languages}")
+
 #Generate the meaning/accuracy/complexity for all languages based on the prior, meaning and Language
 language_data = []
 for language_code in languages:
     language = languages[language_code]
     #Dereference the lang code to get the actual language associated with it
-    language_name = language_codes[language_code] 
+    language_name = language_name_from_code[language_code] 
 
     #Exclude the languages Amuzgo, Camsa, Candoshi, Chayahuita, Chiquitano, Cree, Garífuna (Black Carib), Ifugao, Micmac, Nahuatl, Papago (O’odham), Slave, Tacana, Tarahumara (Central), Tarahumara (Western).
-    #These langauges have fewer than 5 definitions for major terms
+    #These languages have fewer than 5 definitions for major terms
     excluded_language_codes = [7, 19, 20, 25, 27, 31, 38, 48, 70, 78, 80, 88, 91, 92, 93]
 
     if int(language_code) not in excluded_language_codes:
@@ -235,9 +259,15 @@ for language_code in languages:
             #Use just the information bound metric
             language_data.append((language_name, "natural", ib_point[0], ib_point[1], ib_point[2]))
 
+
+
 #Generate some fake languages using the real languages as a baseline via permutation
 #artificial_languages = sampling.get_hypothetical_variants(languages=list(languages.values()), total=400)
-artificial_languages = sampling.random_languages(expressions=expression_set, sampling_strategy="stratified", sample_size=20)
+artificial_languages = sampling.random_languages(expressions=expression_set, sampling_strategy="stratified", sample_size=20, max_size=25)
+#Give enumerated names to each of the artificial languages
+for index, artificial_language in enumerate(artificial_languages):
+    artificial_language.name = f"artificial_lang_{index}"
+
 #Analyze each of the artificial languages
 artificial_lang_count = 0
 for artificial_language in artificial_languages:
@@ -250,8 +280,27 @@ for artificial_language in artificial_languages:
         language_data.append((f"artificial lang {artificial_lang_count}", "artificial", math.log(len(artificial_language.expressions)), artificial_ib_point[1], artificial_ib_point[2]))
     else:
         language_data.append((f"artificial lang {artificial_lang_count}", "artificial", artificial_ib_point[0],  artificial_ib_point[1], artificial_ib_point[2]))
+    
+    #Fill in additional color chips
+    referents_in_language = []
+    for artificial_expression in artificial_language.expressions:
+        referents_in_language += artificial_expression.meaning.referents
+    colors_in_language = [referent.name for referent in referents_in_language]
+    print(f"Artificial language color set size: {len(set(colors_in_language))} colors:{set(colors_in_language)}")
+    if(GENERATE_ADDITIONAL_COLOR_CHIPS):
+        for additional_color in color_universe.referents:
+            if additional_color.name not in colors_in_language:
+                #Get the closest color to the current color
+                closest_color_with_term = min([color_term_ref for color_term_ref in color_universe.referents if color_term_ref.name in colors_in_language],
+                                                key=lambda x: np.linalg.norm(np.array((x.L, x.a, x.b)) - np.array((additional_color.L, additional_color.a, additional_color.b))))
+                closest_expression = get_expression_from_language(closest_color_with_term.name, artificial_language)
+                new_refs = list(closest_expression.meaning.referents)+[additional_color]
+                # Assign the color to the closest expression
+                closest_expression.meaning= Meaning(tuple(new_refs), universe=color_universe)
 
-print(f"Artificial languages{artificial_languages}")
+
+
+#print(f"Artificial languages{artificial_languages}")
 
 #Convert the real and artificial languages to DataFrames
 combined_data = pd.DataFrame(language_data, columns =['name','type','complexity', 'informativity', 'comm_cost'])
@@ -284,22 +333,39 @@ combined_data = pd.DataFrame(language_data, columns =['name','type','speaker_id'
 #Get the IB bound for the specified parameters
 #ib_boundary = rd.get_ib_bound(prior=uniform_prior, meaning_dists=meaning_dists, betas=np.logspace(-2, 2, 10))
 if(GENERATE_IB_BOUND):
-    betas = np.arange(0, 5, .2)
-    if(USE_NOGA_ARRAYS):
-        ib_boundary = rd.get_ib_bound(prior=noga_prior, meaning_dists=noga_meaning_dists)
-    else:
-        ib_boundary = rd.get_ib_bound(prior=uniform_prior, meaning_dists=meaning_dists)
-    ib_boundary_points = pd.DataFrame([("ib_bound", "ib_bound", ib_point.rate, ib_point.accuracy, ib_point.distortion ) 
-                    for ib_point in ib_boundary if ib_point is not None], columns =['name','type','complexity', 'informativity', 'comm_cost'])
+    IB_START = -2
+    IB_STOP = 5
+    IB_STEP = 30
+    betas = np.logspace(IB_START, IB_STOP, IB_STEP)
 
+    #If a cached version of the IB bound already exists, load it
+    ib_bound_filename = f"{current_dir}/outputs/ib_bound_{IB_START}_{IB_STOP}_{IB_STEP}.pkl"
+    if os.path.exists(ib_bound_filename):
+        with open(ib_bound_filename, "rb") as f:
+            print(f"Loading information bound from file:{ib_bound_filename}")
+            ib_boundary = pickle.load(f)
+    else:
+        if(USE_NOGA_ARRAYS):
+            ib_boundary = rd.get_ib_bound(prior=noga_prior, meaning_dists=noga_meaning_dists, betas=betas)
+        else:
+            ib_boundary = rd.get_ib_bound(prior=uniform_prior, meaning_dists=meaning_dists, betas=betas)
+        #Save the IB bound to a file
+        with open(ib_bound_filename, "wb") as f:
+            pickle.dump(ib_boundary, f)
+            
+    ib_boundary_points = pd.DataFrame([("ib_bound", "ib_bound", ib_point.rate, ib_point.accuracy, ib_point.distortion)
+                    for ib_point in ib_boundary if ib_point is not None], columns =['name','type','complexity', 'informativity', 'comm_cost'])
     combined_data = pd.concat([ib_boundary_points, combined_data])
 
+#Combine artificial and natural languages for processing
+languages = languages | {artificial_language.name:artificial_language for artificial_language in artificial_languages}
 
 #Generate plot for color data across languages
 if GENERATE_LANG_COLOR_INFO:
     for language_code in languages:
         language:Language = languages[language_code]
-        language_name = language_codes[language_code]
+        language_name = language.name
+        
         language_color_data = []
         for expression in language.expressions:
             form = expression.form
