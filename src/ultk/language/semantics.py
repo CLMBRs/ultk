da2aa3f6fe50bee558e53b2faba61011e4370d7f
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2024-08-22 14:48:32 -0700

    small style fix in `num_true_like` and format of util

--- src/ultk/language/semantics.py
+++ src/ultk/language/semantics.py
@@ -155,7 +155,7 @@ class Meaning(Generic[T]):
             total_weight = sum(self._dist.values())
             return FrozenDict({referent: weight / total_weight for referent, weight in self._dist.items()})
         else:
-            num_true_like = len(value for value in self.mapping.values() if value)
+            num_true_like = sum(1 for value in self.mapping.values() if value)
             if num_true_like == 0:
                 raise ValueError("Meaning must have at least one true-like referent.")
             return FrozenDict({referent: (1 / num_true_like if self.mapping[referent] else 0) for referent in self.mapping})
