ae0dc50459d25018865b3007a946d1883f3f8d53
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2024-09-12 14:36:23 -0700

    reorder universe by chip number

--- src/examples/colors/meaning.py
+++ src/examples/colors/meaning.py
@@ -6,9 +6,12 @@ from ultk.language.semantics import Universe
 def munsell_cielab_universe(filename: str) -> Universe:
     """
     Load a Universe from a Munsell CIELAB file.
+
+    Note: the tuple Universe.referents will be ordered by the Munsell Chip Number, as used in the WCS data.
     """
     referents = pd.read_csv(filename, delimiter="\t")
-    print(referents)
+    referents.sort_values(by="#cnum", inplace=True)
+    # add a name column, as required by ULTK
     referents["name"] = referents["#cnum"]
     return Universe.from_dataframe(referents)
 
