7dfacf57a1d9232808e33be3884f8ca38f12aade
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2024-09-12 15:55:47 -0700

    natural languages generated

--- src/examples/colors/scripts/read_natural_languages.py
+++ src/examples/colors/scripts/read_natural_languages.py
@@ -1,21 +1,59 @@
+import pickle
+from typing import Any
+
 import pandas as pd
 
 from ultk.language.language import Expression, Language
+from ultk.language.semantics import Meaning
+from ultk.util.frozendict import FrozenDict
+
 from ..meaning import color_universe
 
 if __name__ == "__main__":
 
     term_table = pd.read_csv(
-        "data/term.txt", delimiter="\t", names=("lang", "spkr", "cnum", "term")
+        "colors/data/term.txt", delimiter="\t", names=("lang", "spkr", "cnum", "term")
     )
-    print(term_table)
 
     lang_term_chip_counts = term_table.groupby(["lang", "term", "cnum"]).count()
-    print(lang_term_chip_counts)
-    print(lang_term_chip_counts.index)
 
+    num_referents: int = len(color_universe.referents)
+
+    def safe_loc(df: pd.DataFrame, key: Any, default: Any = 0.0) -> Any:
+        try:
+            return df.loc[key]
+        except KeyError:
+            return default
+
+    languages: list[Language] = []
+    # iterate through each language
     for lang, lang_df in lang_term_chip_counts.groupby("lang"):
-        print(lang)
+        expressions: list[Expression] = []
+        # iterate through each term in each language
         for term, term_df in lang_df.groupby("term"):
-            print(term)
-            print(term_df)
+            # convert counts to probabilities
+            total_count = term_df["spkr"].sum()
+            chip_probabilities = term_df["spkr"] / total_count
+            # Referent: probability mapping, for all Referents = Munsell chips
+            referent_dict = {
+                color_universe.referents[chip_num]: safe_loc(
+                    chip_probabilities, (lang, term, chip_num)
+                )
+                for chip_num in range(num_referents)
+            }
+            # turn into Meaning
+            expression_meaning = Meaning[float](
+                FrozenDict(referent_dict), color_universe, FrozenDict(referent_dict)
+            )
+            # attach form to make Expression
+            expression = Expression(form=f"{lang}-{term}", meaning=expression_meaning)
+            expressions.append(expression)
+        # add Language with its Expressions
+        languages.append(
+            Language(expressions=tuple(expressions), name=lang, natural=True)
+        )
+
+    # write languages to pickle
+    # TODO: serialize in better format (e.g. YAML)
+    with open("colors/outputs/natural_languages.pkl", "wb") as f:
+        pickle.dump(languages, f)
