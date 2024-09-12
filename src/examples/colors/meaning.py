b68da30a223c98e679f6124f51d08237b849ff56
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2024-09-12 15:21:21 -0700

    scripts for color_universe, begin reading natural languages

--- src/examples/colors/meaning.py
+++ src/examples/colors/meaning.py
@@ -1,19 +1,4 @@
-import pandas as pd
+import pickle
 
-from ultk.language.semantics import Universe
-
-
-def munsell_cielab_universe(filename: str) -> Universe:
-    """
-    Load a Universe from a Munsell CIELAB file.
-
-    Note: the tuple Universe.referents will be ordered by the Munsell Chip Number, as used in the WCS data.
-    """
-    referents = pd.read_csv(filename, delimiter="\t")
-    referents.sort_values(by="#cnum", inplace=True)
-    # add a name column, as required by ULTK
-    referents["name"] = referents["#cnum"]
-    return Universe.from_dataframe(referents)
-
-
-color_universe = munsell_cielab_universe("data/cnum-vhcm-lab-new.txt")
+with open("colors/outputs/color_universe.pkl", "rb") as f:
+    color_universe = pickle.load(f)
