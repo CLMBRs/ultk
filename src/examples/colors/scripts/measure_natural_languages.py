82f7f4d0f2c6a1d3dd0f80f36c8d462a658b7978
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2024-09-13 11:30:31 -0700

    single language for testing

--- src/examples/colors/scripts/measure_natural_languages.py
+++ src/examples/colors/scripts/measure_natural_languages.py
@@ -1,14 +1,16 @@
 import pickle
-from yaml import load, Loader
+from yaml import load, Loader, dump, Dumper
 
 from ..ib import language_to_encoder
 
 if __name__ == "__main__":
 
-    with open("colors/outputs/natural_languages.yaml", "r") as f:
-        languages = load(f, Loader=Loader)
+    # with open("colors/outputs/natural_languages.yaml", "r") as f:
+    # languages = load(f, Loader=Loader)
 
-    language = languages[0]
+    with open("colors/outputs/language0.yaml", "r") as f:
+        language = load(f, Loader=Loader)
+
     print(language)
     encoder = language_to_encoder(language)
     print(encoder)
