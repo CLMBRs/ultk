f8f823f76bb2203b85d09fb1d2685f2d09f40924
Ashvin Ranjan <ranjanashvin@gmail.com>
2024-12-02 12:41:33 -0800

    Make changes based on review - Remove `test_meaning_subset` - Remove custom constructor for `Meaning` - Fix issue with custom constructor for `Grammar`

--- src/tests/test_semantics.py
+++ src/tests/test_semantics.py
@@ -52,22 +52,3 @@ class TestSemantics:
     def test_universe_mismatch(self):
         refs = (TestSemantics.ref1,)
         assert Universe(refs) != TestSemantics.universe
-
-    def test_meaning_subset(self):
-        refs = {TestSemantics.ref1: True}
-        meaning = Meaning(
-            refs, TestSemantics.universe
-        )  # This meaning should exist within the set of semantics
-        assert TestSemantics.ref1 in meaning.mapping
-
-        with pytest.raises(ValueError):
-            refs_bad = {
-                TestSemantics.ref1: True,
-                Referent(
-                    name="neutral+epistemic",
-                    properties={"force": "neutral", "flavor": "epistemic"},
-                ): True
-            }
-            meaning = Meaning(
-                refs_bad, TestSemantics.universe
-            )  # This meaning should not
