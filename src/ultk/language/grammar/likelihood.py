9522e4ac8a6e155744aa2e9e8e178ec492dcec3a
Ashvin Ranjan <ranjanashvin@gmail.com>
2025-02-15 17:41:29 -0800

    Add new probability functions for mh_sample - Adds 3 new functions and docstrings   - `percent_match`: The percentage of matching outputs between tree and expected   - `percent_match_unique`: The same as `percent_match` but returns 0 if all items are the same   - `noise_match`: From Piantadosi et al. Treats output as having noise applied and calculated probability that way

--- src/ultk/language/grammar/likelihood.py
+++ src/ultk/language/grammar/likelihood.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Iterable
+from typing import Callable, TypeVar, Iterable
 from ultk.language.grammar.grammar import GrammaticalExpression
 from ultk.language.semantics import Referent
 
@@ -17,3 +17,67 @@ def all_or_nothing(data: Dataset, tree: GrammaticalExpression) -> float:
         float: likelihood
     """
     return float(all(tree(datum[0]) == datum[1] for datum in data))
+
+
+def percent_match(data: Dataset, tree: GrammaticalExpression) -> float:
+    """Basic percentage-based likelihood, returns the percent of matches across the output from the tree
+    and the expected output from the data
+
+    Args:
+        data (Dataset): data for likelihood calculation
+        tree (GrammaticalExpression): GrammaticalExpression for likelihood calculation
+
+    Returns:
+        float: likelihood
+    """
+    return sum([tree(datum[0]) == datum[1] for datum in data])/len(data)
+
+
+def percent_match_unique(data: Dataset, tree: GrammaticalExpression) -> float:
+    """Basic percentage-based likelihood, returns the percent of matches across the output from the tree
+    and the expected output from the data. However, if all of the outputs of the tree are the same returns 0.
+
+    Args:
+        data (Dataset): data for likelihood calculation
+        tree (GrammaticalExpression): GrammaticalExpression for likelihood calculation
+
+    Returns:
+        float: likelihood
+    """
+    first_value = None
+    same = True
+    total_matches = 0
+    for datum in data:
+        val = tree(datum[0])
+        if first_value is None:
+            first_value = val
+        elif same and val != first_value:
+            same = False
+        total_matches += int(val == datum[1])
+    if same:
+        return 0
+    return total_matches/len(data)
+
+
+def noise_match(possible_outputs: int, alpha: float = 0.01) -> Callable[[Dataset, GrammaticalExpression], float]:
+    """Taken from Piantadosi et al. Attempts to discern the probability by believing that the output is correct
+    and was passed through a noise function which has an `alpha` chance to corrupt each item in the output list.
+
+    See also: https://github.com/piantado/LOTlib3/blob/master/Hypotheses/Likelihoods/BinaryLikelihood.py
+
+    Args:
+        possible_ouputs (int): The number of possible values an output is able to be
+        alpha (float): The percentage chance that a value will be mutated
+    
+    Returns:
+        Callable likelihood function:
+            Args:
+                data (Dataset): data for likelihood calculation
+                tree (GrammaticalExpression): GrammaticalExpression for likelihood calculation
+            Returns:
+                float: likelihood
+    """
+    def noise_match_probability(data: Dataset, tree: GrammaticalExpression) -> float:
+        matches = sum([tree(datum[0]) == datum[1] for datum in data])
+        return (alpha/possible_outputs)**(len(data)-matches) * (1-alpha + alpha/possible_outputs)**matches
+    return noise_match_probability
