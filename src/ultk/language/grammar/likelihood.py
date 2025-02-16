5eaa95922f9bb4c678b37c45c34fac8f75f30319
Ashvin Ranjan <ranjanashvin@gmail.com>
2025-02-15 18:01:04 -0800

    Run formatter and add more detail to documentation

--- src/ultk/language/grammar/likelihood.py
+++ src/ultk/language/grammar/likelihood.py
@@ -30,7 +30,7 @@ def percent_match(data: Dataset, tree: GrammaticalExpression) -> float:
     Returns:
         float: likelihood
     """
-    return sum([tree(datum[0]) == datum[1] for datum in data])/len(data)
+    return sum([tree(datum[0]) == datum[1] for datum in data]) / len(data)
 
 
 def percent_match_unique(data: Dataset, tree: GrammaticalExpression) -> float:
@@ -56,19 +56,24 @@ def percent_match_unique(data: Dataset, tree: GrammaticalExpression) -> float:
         total_matches += int(val == datum[1])
     if same:
         return 0
-    return total_matches/len(data)
+    return total_matches / len(data)
 
 
-def noise_match(possible_outputs: int, alpha: float = 0.01) -> Callable[[Dataset, GrammaticalExpression], float]:
+def noise_match(
+    possible_outputs: int, alpha: float = 0.01
+) -> Callable[[Dataset, GrammaticalExpression], float]:
     """Taken from Piantadosi et al. Attempts to discern the probability by believing that the output is correct
     and was passed through a noise function which has an `alpha` chance to corrupt each item in the output list.
+    
+    Takes in the number of possible values the output can be and the percent chance of a corruption and returns a
+    probability function which `mh_sample` is able to use.
 
     See also: https://github.com/piantado/LOTlib3/blob/master/Hypotheses/Likelihoods/BinaryLikelihood.py
 
     Args:
         possible_ouputs (int): The number of possible values an output is able to be
         alpha (float): The percentage chance that a value will be mutated
-    
+
     Returns:
         Callable likelihood function:
             Args:
@@ -77,7 +82,11 @@ def noise_match(possible_outputs: int, alpha: float = 0.01) -> Callable[[Dataset
             Returns:
                 float: likelihood
     """
+
     def noise_match_probability(data: Dataset, tree: GrammaticalExpression) -> float:
         matches = sum([tree(datum[0]) == datum[1] for datum in data])
-        return (alpha/possible_outputs)**(len(data)-matches) * (1-alpha + alpha/possible_outputs)**matches
+        return (alpha / possible_outputs) ** (len(data) - matches) * (
+            1 - alpha + alpha / possible_outputs
+        ) ** matches
+
     return noise_match_probability
