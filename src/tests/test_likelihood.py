86b55b71b6186514d256b3d4d60ddccb80a022a2
Ashvin Ranjan <ranjanashvin@gmail.com>
2025-04-28 15:54:37 -0700

    Reformat test file

--- src/tests/test_likelihood.py
+++ src/tests/test_likelihood.py
@@ -1,17 +1,25 @@
-from ultk.language.grammar.likelihood import all_or_nothing, percent_match_unique, percent_match, noise_match
+from ultk.language.grammar.likelihood import (
+    all_or_nothing,
+    percent_match_unique,
+    percent_match,
+    noise_match,
+)
 from math import log
+
 
 # The expression can simply act as a function for this purpose
 def expression(_):
     return True
 
+
 def even(i):
     return i % 2 == 0
+
 
 class TestLikelihood:
     all_true = [(i, True) for i in range(10)]
     all_false = [(i, False) for i in range(10)]
-    half = [(i, i%2 == 0) for i in range(10)]
+    half = [(i, i % 2 == 0) for i in range(10)]
 
     def test_all_or_nothing(self):
         assert all_or_nothing(self.all_true, expression) == 1
@@ -33,6 +41,17 @@ class TestLikelihood:
 
     def test_noise_match(self):
         noise_match_func = noise_match(2)
-        assert abs(noise_match_func(self.all_true, expression) - log(.995)*10) < 0.00001
-        assert abs(noise_match_func(self.all_false, expression) - log(.005)*10) < 0.00001
-        assert abs(noise_match_func(self.half, expression) - (log(.995)*5 + log(.005)*5)) < 0.00001
+        assert (
+            abs(noise_match_func(self.all_true, expression) - log(0.995) * 10) < 0.00001
+        )
+        assert (
+            abs(noise_match_func(self.all_false, expression) - log(0.005) * 10)
+            < 0.00001
+        )
+        assert (
+            abs(
+                noise_match_func(self.half, expression)
+                - (log(0.995) * 5 + log(0.005) * 5)
+            )
+            < 0.00001
+        )
