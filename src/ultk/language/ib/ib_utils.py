Add expected divergence, fix math, add tests - Add expected KL divergence to IBLanguage - Fix math for I(W; u) in IBLanguage   - Have confirmed that I(M; U) - I(W; U) == E[D[M; M']] - Added tests for IB things   - Very rudementary, will add specific value checks later   - All tests are passing - Reformatted various files

src/ultk/language/ib/ib_utils.py
@@ -1,13 +1,16 @@
 import numpy as np
 
+
 # Turns off warnings for functions which are known to take the log of 0
 def np_log_ignore(f):
     def wrap(*args, **kwargs):
-        np.seterr(divide = 'ignore') 
+        np.seterr(divide="ignore")
         res = f(*args, **kwargs)
-        np.seterr(divide = 'warn')
+        np.seterr(divide="warn")
         return res
+
     return wrap
+
 
 # Take log of array and set all negative infinities to 0
 @np_log_ignore
@@ -16,6 +19,7 @@ def safe_log(arr: np.ndarray):
     A[np.isinf(A)] = 0
     A[np.isnan(A)] = 0
     return A
+
 
 # Calculate the KL Divegence of 2 matricies
 @np_log_ignore