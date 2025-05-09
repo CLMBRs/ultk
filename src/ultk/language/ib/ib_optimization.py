Fix the definition of IBStructure.mu - When being create the transpose was actually being made - This causes the convergence to seem to work properly now   - Normalization is still needed, which is really strange - Tests need to be updated - Structure should probably ensure that there are no 0s in mu

src/ultk/language/ib/ib_optimization.py
@@ -1,10 +1,8 @@
 from ultk.language.ib.ib_language import IBLanguage
 from ultk.language.ib.ib_structure import IBStructure
-from ultk.language.ib.ib_utils import generate_random_expressions, kl_divergence
+from ultk.language.ib.ib_utils import generate_random_expressions, kl_divergence, IB_EPSILON
 
 import numpy as np
-
-EPSILON = 0.00001
 
 
 # Calculate the normal function results for the meanings
@@ -19,8 +17,6 @@ def normal(language: IBLanguage, beta: float) -> np.ndarray:
 
 
 # Do an iteration of the BA Algorithm
-# TODO: For some reason this always converges to a one-expression language
-# Most likely has something to do with the fact normalization is needed
 def recalculate_language(language: IBLanguage, beta: float) -> IBLanguage:
     # Recalculate qwm distribution
     left = language.expressions_prior / normal(language, beta)
@@ -36,7 +32,7 @@ def recalculate_language(language: IBLanguage, beta: float) -> IBLanguage:
     # Recalculate q(w|m)
     recalculated_qwm = left[:, None] * right
     # Drop unused dimensions
-    recalculated_qwm = recalculated_qwm[~np.all(recalculated_qwm <= EPSILON, axis=1)]
+    recalculated_qwm = recalculated_qwm[~np.all(recalculated_qwm <= IB_EPSILON, axis=1)]
     # Normalize (?????) TODO: This should not be needed, investigate
     recalculated_qwm /= np.sum(recalculated_qwm, axis=0)
     # Create new language
@@ -59,7 +55,7 @@ def calculate_optimal(structure: IBStructure, beta: float) -> IBLanguage:
     while not converged:
         old = language.complexity - beta * language.iwu
         language = recalculate_language(language, beta)
-        if abs(language.complexity - beta * language.iwu - old) <= EPSILON:
+        if abs(language.complexity - beta * language.iwu - old) <= IB_EPSILON:
             converged = True
         old = language.complexity - beta * language.iwu
 

src/ultk/language/ib/ib_structure.py
@@ -45,8 +45,8 @@ class IBStructure:
         # I do not want to assume everything is in a nice order
         return np.array(
             [
-                [meaning.mapping[referent] for referent in self.referents]
-                for meaning in self.meanings
+                [meaning.mapping[referent] for meaning in self.meanings]
+                for referent in self.referents
             ]
         )
 

src/ultk/language/ib/ib_utils.py
@@ -3,6 +3,7 @@ import numpy as np
 from ultk.language.semantics import Meaning
 from ultk.util.frozendict import FrozenDict
 
+IB_EPSILON = 0.00001
 
 # Turns off warnings for functions which are known to take the log of 0
 def np_log_ignore(f):
@@ -25,9 +26,10 @@ def safe_log(arr: np.ndarray):
 
 
 # Calculate the KL Divegence of 2 matricies
-# TODO: For some reason this gives a different result than scipy.stats' entropy
 @np_log_ignore
 def kl_divergence(arr1: np.ndarray, arr2: np.ndarray) -> float:
+    if (abs(np.sum(arr1) - 1) > IB_EPSILON or abs(np.sum(arr2) - 1) > IB_EPSILON):
+        raise ValueError("Arrays are not probability distributions")
     return np.sum(arr1 * safe_log(arr1 / arr2))
 
 